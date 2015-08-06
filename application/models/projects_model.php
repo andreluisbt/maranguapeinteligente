739f374882158a04cbf678ca76517a4dfd2386a8
Andre Luis <andreluisbt@gmail.com>
2015-08-06 11:41:08 -0300

    ---

--- application/models/projects_model.php
+++ application/models/projects_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class ProjectsModel extends CI_Model {
+class Projects_Model extends CI_Model {
 
 	public $id;
 	public $category;
