54511003ac06d9ce243b7dd988c110a6bb18168b
André Luis <andreluisbt@gmail.com>
2015-09-08 22:01:16 -0300

    ---

--- maranguapeinte.sql
+++ maranguapeinte.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 03-Set-2015 às 02:25
+-- Generation Time: 09-Set-2015 às 03:00
 -- Versão do servidor: 5.6.25
 -- PHP Version: 5.6.11
 
@@ -34,7 +34,32 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `accepted` int(11) NOT NULL,
   `timecreated` int(11) NOT NULL,
   `timeaccepted` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=828 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `comments`
+--
+
+INSERT INTO `comments` (`id`, `comment`, `project_id`, `user_id`, `accepted`, `timecreated`, `timeaccepted`) VALUES
+(1, 'sdasdadaksdandnlkjasdnj adasdd adsd\r\nasd \r\nasd\r\nasd\r\nasasddsadsasdadsd ssaasfsffdgdfgdfg\r\n dfgdfgdfgdfgdf\r\ngdf\r\ngdfgdfgdgdg dfg', 63, 50, 0, 0, 0),
+(2, 'sdasdadaksdandnlkjasdnj adasdd adsd\r\nasd \r\nasd\r\nasd\r\nasasddsadsasdadsd ssaasfsffdgdfgdfg\r\n dfgdfgdfgdfgdf\r\ngdf\r\ngdfgdfgdgdg dfg', 63, 50, 0, 11111, 222222),
+(3, 'sdasdadaksdandnlkjasdnj adasdd adsd\r\nasd \r\nasd\r\nasd\r\nasasddsadsasdadsd ssaasfsffdgdfgdfg\r\n dfgdfgdfgdfgdf\r\ngdf\r\ngdfgdfgdgdg dfg', 63, 50, 0, 11111, 222222),
+(4, 'sdasdadaksdandnlkjasdnj adasdd adsd\r\nasd \r\nasd\r\nasd\r\nasasddsadsasdadsd ssaasfsffdgdfgdfg\r\n dfgdfgdfgdfgdf\r\ngdf\r\ngdfgdfgdgdg dfg', 63, 50, 0, 11111, 222222),
+(5, 'sdasdadaksdandnlkjasdnj adasdd adsd\n\nasd \r\nasd\r\nasd\r\nasasddsadsasdadsd ssaasfsffdgdfgdfg\r\n dfgdfgdfgdfgdf\r\ngdf\r\ngdfgdfgdgdg dfg', 63, 50, 0, 11111, 222222),
+(6, 'sdasdadaksdandnlkjasdnj adasdd adsd\r\nasd \r\nasd\r\nasd\r\nasasddsadsasdadsd ssaasfsffdgdfgdfg\r\n dfgdfgdfgdfgdf\r\ngdf\r\ngdfgdfgdgdg dfg', 63, 50, 0, 11111, 222222),
+(7, 'sdasdadaksdandnlkjasdnj adasdd adsd\r\nasd \r\nasd\r\nasd\r\nasasddsadsasdadsd ssaasfsffdgdfgdfg\r\n dfgdfgdfgdfgdf\r\ngdf\r\ngdfgdfgdgdg dfg', 63, 50, 0, 11111, 222222),
+(8, 'asda sdasda\r\nsd\r\nasd\r\nasdasd asd a sdasd\r\ndas \r\nasd \r\nsdaasdasd', 63, 49, 0, 987987, 98),
+(798, '', 0, 0, 0, 0, 0),
+(811, 'asda sdasda sd asd asdasd asd a sdasd das asd sdaasdasd', 63, 49, 0, 987987, 98),
+(819, 'asda sdasda sd asd asdasd asd a sdasd das asd sdaasdasd', 63, 49, 0, 987987, 98),
+(820, 'asda sdasda sd asd asdasd asd a sdasd das asd sdaasdasd', 63, 49, 0, 987987, 98),
+(821, 'asda sdasda sd asd asdasd asd a sdasd das asd sdaasdasd', 63, 49, 0, 987987, 98),
+(822, 'asda sdasda sd asd asdasd asd a sdasd das asd sdaasdasd', 63, 49, 0, 987987, 98),
+(823, 'asda sdasda sd asd asdasd asd a sdasd das asd sdaasdasd', 63, 49, 0, 987987, 98),
+(824, 'asda sdasda sd asd asdasd asd a sdasd das asd sdaasdasd', 63, 49, 0, 987987, 98),
+(825, 'asda sdasda sd asd asdasd asd a sdasd das asd sdaasdasd', 63, 49, 0, 987987, 98),
+(826, 'asda sdasda sd asd asdasd asd a sdasd das asd sdaasdasd', 63, 49, 0, 987987, 98),
+(827, 'asda sdasda sd asd asdasd asd a sdasd das asd sdaasdasd', 63, 49, 0, 987987, 98);
 
 -- --------------------------------------------------------
 
@@ -52,7 +77,7 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `lng` varchar(50) NOT NULL,
   `date` varchar(250) NOT NULL,
   `publish` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `projects`
@@ -62,7 +87,15 @@ INSERT INTO `projects` (`id`, `owner`, `category`, `title`, `description`, `lat`
 (62, 50, 'Cultura', 'Teatro nos bairos', 'Apresentações de teatro nas ruas da cidade com peças que contam a história dos bairro onde está acontecendo a apresentação', '', '', '1439609121', 1),
 (63, 49, 'Reforma', 'Reforma Igreja de Nossa Senhora do Perpétuo Socorro', 'A Igreja de Nossa Senhora do Perpétuo Socorro representa um ícone na comunidade de coqueirinhos e em nossa cidade, por isso deve receber maiores cuidados por parte de nossa prefeitura', '-3.8944004697310124', '-38.68724137544632', '1439610264', 1),
 (64, 49, 'Reforma', 'Reforma Igreja de Nossa Senhora do Perpétuo Socorro 2', 'A Igreja de Nossa Senhora do Perpétuo Socorro representa um ícone na comunidade de coqueirinhos e em nossa cidade, por isso deve receber maiores cuidados por parte de nossa prefeitura', '-3.8944004697310124', '-38.68724137544632', '1439610264', 1),
-(65, 50, 'Cultura', 'Teatro nos bairos2', 'Apresentações de teatro nas ruas da cidade com peças que contam a história dos bairro onde está acontecendo a apresentação', '', '', '1439609121', 1);
+(65, 50, 'Cultura', 'Teatro nos bairos2', 'Apresentações de teatro nas ruas da cidade com peças que contam a história dos bairro onde está acontecendo a apresentação', '', '', '1439609121', 1),
+(66, 50, 'Cultura', 'Teatro nos bairos', 'Apresentações de teatro nas ruas da cidade com peças que contam a história dos bairro onde está acontecendo a apresentação', '', '', '1439609121', 1),
+(67, 49, 'Reforma', 'Reforma Igreja de Nossa Senhora do Perpétuo Socorro', 'A Igreja de Nossa Senhora do Perpétuo Socorro representa um ícone na comunidade de coqueirinhos e em nossa cidade, por isso deve receber maiores cuidados por parte de nossa prefeitura', '-3.8944004697310124', '-38.68724137544632', '1439610264', 1),
+(68, 49, 'Reforma', 'Reforma Igreja de Nossa Senhora do Perpétuo Socorro 2', 'A Igreja de Nossa Senhora do Perpétuo Socorro representa um ícone na comunidade de coqueirinhos e em nossa cidade, por isso deve receber maiores cuidados por parte de nossa prefeitura', '-3.8944004697310124', '-38.68724137544632', '1439610264', 1),
+(69, 50, 'Cultura', 'Teatro nos bairos2', 'Apresentações de teatro nas ruas da cidade com peças que contam a história dos bairro onde está acontecendo a apresentação', '', '', '1439609121', 1),
+(70, 50, 'Cultura', 'Teatro nos bairos', 'Apresentações de teatro nas ruas da cidade com peças que contam a história dos bairro onde está acontecendo a apresentação', '', '', '1439609121', 1),
+(71, 49, 'Reforma', 'Reforma Igreja de Nossa Senhora do Perpétuo Socorro', 'A Igreja de Nossa Senhora do Perpétuo Socorro representa um ícone na comunidade de coqueirinhos e em nossa cidade, por isso deve receber maiores cuidados por parte de nossa prefeitura', '-3.8944004697310124', '-38.68724137544632', '1439610264', 1),
+(72, 49, 'Reforma', 'Reforma Igreja de Nossa Senhora do Perpétuo Socorro 2', 'A Igreja de Nossa Senhora do Perpétuo Socorro representa um ícone na comunidade de coqueirinhos e em nossa cidade, por isso deve receber maiores cuidados por parte de nossa prefeitura', '-3.8944004697310124', '-38.68724137544632', '1439610264', 1),
+(73, 50, 'Cultura', 'Teatro nos bairos2', 'Apresentações de teatro nas ruas da cidade com peças que contam a história dos bairro onde está acontecendo a apresentação', '', '', '1439609121', 1);
 
 -- --------------------------------------------------------
 
@@ -76,7 +109,14 @@ CREATE TABLE IF NOT EXISTS `rates` (
   `project_id` int(11) NOT NULL,
   `rate` int(11) NOT NULL,
   `timecreated` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `rates`
+--
+
+INSERT INTO `rates` (`id`, `user_id`, `project_id`, `rate`, `timecreated`) VALUES
+(87, 50, 62, 1, 1441240819);
 
 -- --------------------------------------------------------
 
@@ -97,7 +137,8 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 --
 
 INSERT INTO `sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('9f5aa8990e6ba21e7122f46bddb3dff5', '::1', 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 1441238441, 'a:2:{s:9:"user_data";s:0:"";s:4:"user";O:8:"stdClass":11:{s:2:"id";s:2:"50";s:16:"represents_group";s:1:"0";s:10:"group_name";N;s:4:"name";s:11:"André Luis";s:5:"email";s:19:"andreluis@email.com";s:4:"born";s:10:"1980-06-17";s:6:"gender";s:1:"0";s:7:"address";s:31:"Rua Dom Quintino, 276 - Pirambu";s:3:"cpf";s:14:"123.123.123-12";s:5:"image";s:36:"ca8b008c2d019ec53cb43c52841c3d1c.jpg";s:11:"timecreated";s:10:"1439607287";}}');
+('44506068e895a7ca8028ffd31b0ded59', '::1', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36', 1441756574, ''),
+('b23c2f41604db5189127aaa03b02e6a4', '::1', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36', 1441760237, 'a:1:{s:4:"user";O:8:"stdClass":11:{s:2:"id";s:2:"50";s:16:"represents_group";s:1:"0";s:10:"group_name";N;s:4:"name";s:11:"André Luis";s:5:"email";s:19:"andreluis@email.com";s:4:"born";s:10:"1980-06-17";s:6:"gender";s:1:"0";s:7:"address";s:31:"Rua Dom Quintino, 276 - Pirambu";s:3:"cpf";s:14:"123.123.123-12";s:5:"image";s:36:"ca8b008c2d019ec53cb43c52841c3d1c.jpg";s:11:"timecreated";s:10:"1439607287";}}');
 
 -- --------------------------------------------------------
 
@@ -171,17 +212,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=828;
 --
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=66;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=74;
 --
 -- AUTO_INCREMENT for table `rates`
 --
 ALTER TABLE `rates`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=87;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=88;
 --
 -- AUTO_INCREMENT for table `users`
 --
